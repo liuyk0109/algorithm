a5bafd7b34aefe5250aab2080a5696042c51ec2c
liuyikang <liuyk0109@gmail.com>
2020-11-18 01:07:12 +0800

    自底向上的归并排序

--- src/main/java/com/thelyk/algorithm/sort/MergeBU.java
+++ src/main/java/com/thelyk/algorithm/sort/MergeBU.java
@@ -1,20 +1,15 @@
 package com.thelyk.algorithm.sort;
 
-public class Merge {
+public class MergeBU {
 
     public static void sort(Comparable[] a) {
-        Comparable[] aux = new Comparable[a.length];
-        sort(a, aux, 0, a.length - 1);
-    }
-
-    private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
-        if (hi <= lo) {
-            return;
+        int n = a.length;
+        Comparable[] aux = new Comparable[n];
+        for (int sz = 1; sz < n; sz = sz + sz) {
+            for (int lo = 0; lo < n - sz; lo += sz + sz) {
+                merge(a, aux, lo, lo + sz - 1, Math.min(lo + sz + sz - 1, n - 1));
+            }
         }
-        int mid = lo + (hi - lo) / 2;
-        sort(a, aux, lo, mid);
-        sort(a, aux, mid + 1, hi);
-        merge(a, aux, lo, mid, hi);
     }
 
     private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
